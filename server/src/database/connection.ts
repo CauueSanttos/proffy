cbc026c92431f1416f856f5b9639be4012ab444e
Caue dos Santos Silva <cauedossantossilva@hotmail.com>
2020-09-27 11:25:51 -0300

    feat: Configuração do backend/rotas

---
+++ server/src/database/connection.ts
@@ -0,0 +1,13 @@
+import { response } from 'express';
+import knex from 'knex';
+import { resolve } from 'path';
+
+const db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: resolve(__dirname, 'database.sqlite'),
+  },
+  useNullAsDefault: true,
+});
+
+export default db;
